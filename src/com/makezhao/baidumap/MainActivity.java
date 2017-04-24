df5109fe1b83ab80dca1066624298d6bb2257cb4
makezhao <1170655282@qq.com>
2017-04-24 22:46:16 +0800

    百度地图接入

---
+++ src/com/makezhao/baidumap/MainActivity.java
@@ -0,0 +1,191 @@
+package com.makezhao.baidumap;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
+
+public class MainActivity extends Activity {
+	private MapView mMapView;
+	private BaiduMap mBaiduMap;
+	private Context mContext;
+
+	// 定位相关
+	private LocationClient mLocationClient;
+	private MyLocationListener myLocationListener;
+	private boolean isFirstIn = true;
+	private double mLatitude;
+	private double mLongtitude;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		// 在使用SDK各组件之前初始化context信息，传入ApplicationContext
+		// 注意该方法要再setContentView方法之前实现
+		SDKInitializer.initialize(getApplicationContext());
+		// requestWindowFeature(Window.FEATURE_NO_TITLE);
+		this.mContext = this;
+		setContentView(R.layout.activity_main);
+		initView();
+
+		// 初始化定位
+		initLocation();
+	}
+
+	private void initLocation() {
+		// TODO Auto-generated method stub
+		mLocationClient = new LocationClient(this);
+		myLocationListener = new MyLocationListener();
+		mLocationClient.registerLocationListener(myLocationListener);
+
+		LocationClientOption option = new LocationClientOption();
+		option.setCoorType("bd09ll");
+		option.setIsNeedAddress(true);
+		option.setOpenGps(true);
+		option.setScanSpan(1000);// 每隔多少秒进行一次请求
+
+		mLocationClient.setLocOption(option);
+	}
+
+	private void initView() {
+		// TODO Auto-generated method stub
+		mMapView = (MapView) findViewById(R.id.id_bmapView);
+		mBaiduMap = mMapView.getMap();
+		MapStatusUpdate msu = MapStatusUpdateFactory.zoomTo(15.0f);
+		mBaiduMap.setMapStatus(msu);
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		// 在activity执行onDestroy时执行mMapView.onDestroy()，实现地图生命周期管理
+		mMapView.onDestroy();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		// 在activity执行onResume时执行mMapView. onResume ()，实现地图生命周期管理
+		mMapView.onResume();
+	}
+
+	@Override
+	protected void onStart() {
+		// TODO Auto-generated method stub
+		super.onStart();
+		// 开始定位
+		mBaiduMap.setMyLocationEnabled(true);
+		if (!mLocationClient.isStarted()) {
+			mLocationClient.start();
+		}
+	}
+
+	@Override
+	protected void onPause() {
+		super.onPause();
+		// 在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+		mMapView.onPause();
+	}
+
+	@Override
+	protected void onStop() {
+		// TODO Auto-generated method stub
+		super.onStop();
+		// 停止定位
+		mBaiduMap.setMyLocationEnabled(false);
+		mLocationClient.stop();
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// TODO Auto-generated method stub
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		// TODO Auto-generated method stub
+		switch (item.getItemId()) {
+		case R.id.id_map_common:
+			mBaiduMap.setMapType(BaiduMap.MAP_TYPE_NORMAL);
+			break;
+		case R.id.id_map_site:
+			mBaiduMap.setMapType(BaiduMap.MAP_TYPE_SATELLITE);
+			break;
+		case R.id.id_map_traffic:
+			if (mBaiduMap.isTrafficEnabled()) {
+				mBaiduMap.setTrafficEnabled(false);
+				item.setTitle("实时交通(off)");
+			} else {
+				mBaiduMap.setTrafficEnabled(true);
+				item.setTitle("实时交通(on)");
+			}
+			break;
+		case R.id.id_map_location:
+			centerToMyLocation();
+			break;
+
+		default:
+			break;
+		}
+		return super.onOptionsItemSelected(item);
+	}
+
+	// 定位到我的位置
+	private void centerToMyLocation() {
+		LatLng latLng = new LatLng(mLatitude, mLongtitude);
+		MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+		mBaiduMap.animateMapStatus(msu);
+	}
+
+	private class MyLocationListener implements BDLocationListener {
+
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			// TODO Auto-generated method stub
+			MyLocationData data = new MyLocationData.Builder()//
+					.accuracy(location.getRadius())//
+					.latitude(location.getLatitude())//
+					.longitude(location.getLongitude())//
+					.build();
+			mBaiduMap.setMyLocationData(data);
+			// MyLocationConfiguration config=new
+			// MyLocationConfiguration(LocationMode.NORMAL, arg1, arg2);
+
+			mLatitude = location.getLatitude();
+			mLongtitude = location.getLongitude();
+
+			if (isFirstIn) {
+				LatLng latLng = new LatLng(location.getLatitude(),
+						location.getLongitude());
+				MapStatusUpdate msu = MapStatusUpdateFactory.newLatLng(latLng);
+				mBaiduMap.animateMapStatus(msu);
+				isFirstIn = false;
+
+				Toast.makeText(mContext, location.getAddrStr(),
+						Toast.LENGTH_SHORT).show();
+			}
+		}
+
+		@Override
+		public void onConnectHotSpotMessage(String arg0, int arg1) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+}
